6512f3efb95b467422c4c747854b33b9acf30b9c
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2018-03-23 15:54:14 +0530

    feat: create action from msg with type extracted from msg's ctor values

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import RemoteDev from 'remotedev';
 
 const remotedev = RemoteDev.connectViaExtension();
 
+const toAction = msg => {
+  const type = msg => (msg && msg.ctor ? `${msg.ctor}:${type(msg._0)}` : '');
+  return {
+    type: type(msg).slice(0, -1),
+    ...msg,
+  };
+};
+
 export const send = (msg, model) => {
-  const action = msg;
+  const action = toAction(msg);
   const state = model;
   remotedev.send(action, state);
 };
